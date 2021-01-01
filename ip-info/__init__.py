4b437732935b9c57cbff181f928111754b13f73f
Ilya <gorunovila82@gmail.com>
2021-01-01 20:16:45 +0300

    2

---
+++ ip-info/__init__.py
@@ -0,0 +1,20 @@
+import requests
+
+class IPDataError(Exception):
+        errno: int
+        strerror: str
+
+class ip():
+    ip = "1"
+    def __init__(self,ip:str):
+        self.ip = ip
+
+    def get_data(self):
+        r = requests.get(f"http://ip-api.com/json/{self.ip}").json()
+        if r["status"] == "fail":
+            raise IPDataError("fail,try again!")
+        else:
+            self.res = r 
+            return r
+
+ip("178.162.32.11").get_data()
